e6de79cf0738efc2a2f093c2c470bd9652c9d2ad
Marvin Lindner <se24m035@technikum-wien.at>
2025-05-07 19:20:33 +0200

    added springboot backend

---
+++ server/src/main/java/at/fhtw/mse/awt/tewntyone/TewntyoneApplication.java
@@ -0,0 +1,13 @@
+package at.fhtw.mse.awt.tewntyone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TewntyoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TewntyoneApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/at/fhtw/mse/awt/tewntyone/TewntyoneApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhtw.mse.awt.tewntyone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TewntyoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
