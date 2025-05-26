1572fa7a91f83038b0f98be8e4838e73f72b3304
Marvin Lindner <se24m035@technikum-wien.at>
2025-05-26 21:00:22 +0200

    [FE] generated frontend api

---
+++ client/src/components/game/DealerHand.vue
@@ -0,0 +1,20 @@
+<template>
+  <IonCard>
+    <IonCardHeader>
+      <IonCardTitle>Dealer</IonCardTitle>
+    </IonCardHeader>
+    <IonCardContent>
+      <IonRow>
+        <IonCol v-for="(card, idx) in cards" :key="idx">
+          {{ card }}
+        </IonCol>
+      </IonRow>
+    </IonCardContent>
+  </IonCard>
+</template>
+
+<script setup lang="ts">
+import { IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCol, IonRow } from '@ionic/vue'
+
+defineProps<{ cards: string[] }>()
+</script>
