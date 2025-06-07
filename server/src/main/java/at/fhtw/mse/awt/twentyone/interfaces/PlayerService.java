48801734cb9944e5209aa77ba51203827ccb91c1
ninnaah <nina.hochmayr4@gmail.com>
2025-06-07 18:03:49 +0200

    add playerService

---
+++ server/src/main/java/at/fhtw/mse/awt/twentyone/interfaces/PlayerService.java
@@ -0,0 +1,42 @@
+package at.fhtw.mse.awt.twentyone.interfaces;
+
+import at.fhtw.mse.awt.twentyone.dtos.Player.PlayerDto;
+import at.fhtw.mse.awt.twentyone.dtos.Player.PlayerRequestDto;
+
+/**
+ * Service interface for retrieving player profile.
+ */
+public interface PlayerService {
+
+    /**
+     * Retrieves the player profile.
+     *
+     * @param playerId of the player
+     * @return DTO with player data
+     */
+    PlayerDto getPlayer(Long playerId);
+
+    /**
+     * Creates a new player profile.
+     *
+     * @param playerRequestDto Request DTO with player data
+     * @return DTO of the created player with generated ID
+     */
+    PlayerDto createPlayer(PlayerRequestDto playerRequestDto);
+
+    /**
+     * Updates an existing player profile.
+     *
+     * @param playerId ID of the player to update
+     * @param playerDto DTO with updated player data
+     * @return DTO of the updated player
+     */
+    PlayerDto updatePlayer(Long playerId, PlayerRequestDto playerDto);
+
+    /**
+     * Deletes a player profile.
+     *
+     * @param playerId ID of the player to delete
+     */
+    void deletePlayer(Long playerId);
+}
---
+++ server/src/main/java/at/fhtw/mse/awt/twentyone/services/PlayerServiceImpl.java
@@ -0,0 +1,75 @@
+package at.fhtw.mse.awt.twentyone.services;
+
+import at.fhtw.mse.awt.twentyone.dtos.Player.PlayerDto;
+import at.fhtw.mse.awt.twentyone.dtos.Player.PlayerRequestDto;
+import at.fhtw.mse.awt.twentyone.entities.Player;
+import at.fhtw.mse.awt.twentyone.interfaces.PlayerService;
+import at.fhtw.mse.awt.twentyone.repositories.PlayerRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+public class PlayerServiceImpl implements PlayerService {
+
+    private final PlayerRepository playerRepository;
+
+    // Constructor injection for the repository
+    public PlayerServiceImpl(PlayerRepository playerRepository) {
+        this.playerRepository = playerRepository;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public PlayerDto getPlayer(Long playerId) {
+        Player player = playerRepository.findById(playerId)
+                .orElseThrow(() -> new EntityNotFoundException("Player not found with id: " + playerId));
+        return mapToDto(player);
+    }
+
+    @Override
+    public PlayerDto createPlayer(PlayerRequestDto playerRequestDto) {
+        Player playerToCreate = mapToEntity(playerRequestDto);
+        Player player = playerRepository.save(playerToCreate);
+        return mapToDto(player);
+    }
+
+    @Override
+    public PlayerDto updatePlayer(Long playerId, PlayerRequestDto playerRequestDto) {
+        Player playerToCreate = mapToEntity(playerRequestDto);
+        Player player = playerRepository.save(playerToCreate);
+        return mapToDto(player);
+    }
+
+    @Override
+    public void deletePlayer(Long playerId) {
+        playerRepository.removeByPlayerId(playerId)
+                .orElseThrow(() -> new EntityNotFoundException("Player not found with id: " + playerId));
+    }
+
+    private PlayerDto mapToDto(Player player) {
+        if (player == null) {
+            return null;
+        }
+        return new PlayerDto(
+                player.getPlayerId(),
+                player.getUserName(),
+                player.getName()
+        );
+    }
+
+    private Player mapToEntity(PlayerRequestDto player) {
+        if (player == null) {
+            return null;
+        }
+        return new Player(
+                player.getId(),
+                player.getUserName(),
+                player.getName(),
+                player.getPasswordHash()
+        );
+    }
+
+
+}
