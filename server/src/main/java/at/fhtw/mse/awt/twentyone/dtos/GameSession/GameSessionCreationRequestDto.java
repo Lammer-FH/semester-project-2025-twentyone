360f3e4ba55659e143ba3df56be8e11a4754efea
Cadezzz <cader.reynaud@gmail.com>
2025-06-08 08:31:12 +0200

    feat: Implement GameSession controller and service

--- server/src/main/java/at/fhtw/mse/awt/twentyone/dtos/GameSession/GameSessionCreationRequestDto.java
+++ server/src/main/java/at/fhtw/mse/awt/twentyone/dtos/GameSession/GameSessionCreationRequestDto.java
@@ -4,16 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-// @Builder // <-- REMOVED
-public class GameSessionDto {
-    private Long id;
+public class GameSessionCreationRequestDto {
     private Long playerId;
-    private String status;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
 }
--- server/src/main/java/at/fhtw/mse/awt/twentyone/dtos/GameSession/GameSessionUpdateRequestDto.java
+++ server/src/main/java/at/fhtw/mse/awt/twentyone/dtos/GameSession/GameSessionUpdateRequestDto.java
@@ -4,16 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-// @Builder // <-- REMOVED
-public class GameSessionDto {
-    private Long id;
-    private Long playerId;
+public class GameSessionUpdateRequestDto {
     private String status;
-    private LocalDateTime startTime;
-    private LocalDateTime endTime;
 }
