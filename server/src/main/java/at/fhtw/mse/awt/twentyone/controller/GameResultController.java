a7b35a3bc46fe2252ba84c0600f03cba4496ff53
Marvin Lindner <marvin.lindner@pv.at>
2025-06-10 14:20:10 +0200

    CR: Codecleanup

--- server/src/main/java/at/fhtw/mse/awt/twentyone/controller/GameResultController.java
+++ server/src/main/java/at/fhtw/mse/awt/twentyone/controller/GameResultController.java
@@ -16,8 +16,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-// import java.util.List; // No longer returning a list
-
 @RestController
 @RequestMapping("/game-results")
 @Tag(name = "Game Results", description = "Result for a completed game session")
